98bb01c24491ccf3826a2dd36c69a40e2c05813e
Bailey Shelden <baileyshelden@Baileys-MacBook-Air-2.local>
2019-06-21 11:41:36 -0600

    fourth commit

--- homepage.js
+++ homepage.js
@@ -4,16 +4,22 @@ var wednesdaycontainer = $('.wednesday-container');
 var thursdaycontainer = $('.thursday-container');
 var fridaycontainer = $('.friday-container');
 var saturdaycontainer = $('.saturday-container');
-var sundaycontainer = $('.sunday-container')
+var sundaycontainer = $('.sunday-container');
+var generalcontainer = $('.general-container');
 var btn = $('.submit-btn');
+var deleteone = $('.deleteone')
+var namecard = $('.name-card')
 // var deleteall = $('.delete');
 
 btn.on("click", checkDay);
+// deleteone.on("click", strikeOne);
+// $('.deleteone').on("click", strikeOne()) ;
 
 function checkDay(event){
  event.preventDefault();
  var day = $('.info-stuff').val();
-  var name = $('.name').val();
+ var name = $('.name').val();
+
 
   if(day=="monday"){
   appendDate(mondaycontainer, name);
@@ -29,13 +35,16 @@ function checkDay(event){
     appendDate(saturdaycontainer, name);
   } else if(day=="sunday") {
     appendDate(sundaycontainer, name);
+  } else if(day=="general"){
+    appendDate(generalcontainer, name);
   } else{
     appendError();
   }
 }
 
 function appendDate(dayContainer, name) {
-  dayContainer.append(`<p class="name-card"> -${name} </p>`);
+  dayContainer.append(`<div class="strikethrough"><p class="name-card nostrike"> -${name} </p> </div>`);
+  // <input class="deleteone" type="submit" value="delete">
     $('.name').val("");
     $('.info-stuff').val("");
 }
@@ -44,7 +53,22 @@ function appendError(){
   $('.name').val("");
   $('.info-stuff').val("");
 }
+
+function deleteEverything() {
+  $(".name-card").remove();
+}
+
+// function strikeOne() {
+//   console.log("hello");
+//   // var element = $("strikethrough");
+//   //   element.classList.addClass('strike');
+//   //   element.classList.removeClass('nostrike');
+//   // let el=document.querySelector('.target');
+//   // el.style.setProperty('text-decoration', 'line-through')
+// }
 //
-// function deleteEverything() {
-//   $(".name-card").remove();
+// function strikeOne(event){
+//   event.preventDefault()
+//   console.log("hello");
+// // $('.strikethrough').innerText(`<p class="name-strike"> <strike>-${name} </strike></p> <input class="deleteone" type="submit" value="delete">`)
 // }
